76622a1c5970aff119ae5c1663fb4c76ed7a2ddb
Aili Wusiman <aliosmancanada@gmail.com>
2023-10-01 10:03:16 -0400

    added new code

--- src/task1/Test1.java
+++ src/task1/Test1.java
@@ -3,5 +3,6 @@ package task1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("test 1 is started");
+        System.out.println("git commit");
     }
 }
