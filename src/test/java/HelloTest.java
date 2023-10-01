d0c3488956084c06e1527b51a8500d51fba03398
Aili Wusiman <aliosmancanada@gmail.com>
2023-10-01 12:30:12 -0400

    feature A

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("created Test file");
+        System.out.println("feature A is created");
+
     }
 }
